9088c4aeeb95b0a22b504d99919d3ae4f3cef72b
Delario4994 <denance@davidson.edu>
2023-01-28 22:56:39 -0500

    passed Getting Size unit tests for ArrayList

--- src/ArrayList.py
+++ src/ArrayList.py
@@ -3,7 +3,7 @@ A Python program containing the class definition for my implementation
 of the ArrayList data structure using test-driven development (TDD).
 
 Author: Delario Nance, Jr.
-Date: January 24, 2023 - January 27, 2023
+Date: January 24, 2023 - January 28, 2023
 """
 
 # Standard library imports
@@ -16,10 +16,12 @@ class ArrayList:
     """_summary_
     """
     def __init__(self, values: list[float]) -> None:
-        """Constructs an ArrayList object from a user-specified list of numbers.
+        """Constructs an ArrayList object from a 
+           user-specified list of numbers.
 
         Args:
-            values (list[float]): The user-specified list of numbers
+            values (list[float]): The user-specified list of 
+                                  numbers
         """
         self._size = len(values)
         
--- testing/unit/ArrayList_unit_test.py
+++ testing/unit/ArrayList_unit_test.py
@@ -3,7 +3,7 @@ A Python script for unit testing the ArrayList class with
 PyTest.
 
 Author: Delario Nance, Jr.
-Date: January 24, 2023 - January 27, 2023
+Date: January 24, 2023 - January 28, 2023
 """
 
 # Standard library imports
@@ -21,27 +21,29 @@ from helpers import rand_array_list
 
 
 class TestGettingSize:
+    """Uses __init__ and __len__ methods from ArrayList class.
+    """
     def test_get_size_of_empty_ArrayList(self):
         empty_arraylist = rand_array_list(0)
         size = len(empty_arraylist)
         assert size == 0
     
     def test_get_size_of_ArrayList_of_one_int(self):
-        empty_arraylist = rand_array_list(1)
-        size = len(empty_arraylist)
+        arraylist_of_one_int = rand_array_list(1)
+        size = len(arraylist_of_one_int)
         assert size == 1
     
     def test_get_size_of_ArrayList_of_ten_ints(self):
-        empty_arraylist = rand_array_list(10)
-        size = len(empty_arraylist)
+        arraylist_of_ten_ints = rand_array_list(10)
+        size = len(arraylist_of_ten_ints)
         assert size == 10
     
     def test_get_size_of_ArrayList_of_one_hundred_ints(self):
-        empty_arraylist = rand_array_list(100)
-        size = len(empty_arraylist)
+        arraylist_of_one_hundred_ints = rand_array_list(100)
+        size = len(arraylist_of_one_hundred_ints)
         assert size == 100
     
     def test_get_size_of_ArrayList_of_ten_thousand_ints(self):
-        empty_arraylist = rand_array_list(10000)
-        size = len(empty_arraylist)
+        arraylist_of_ten_thousand_ints = rand_array_list(10000)
+        size = len(arraylist_of_ten_thousand_ints)
         assert size == 10000
