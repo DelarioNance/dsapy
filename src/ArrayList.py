728f33174da87d4fe1f3cd0cef7a61ff7579993a
Delario4994 <denance@davidson.edu>
2023-01-28 22:51:08 -0500

    wrote untested __init__ and __len__ methods for ArrayList

--- src/ArrayList.py
+++ src/ArrayList.py
@@ -15,3 +15,18 @@ import numpy as np
 class ArrayList:
     """_summary_
     """
+    def __init__(self, values: list[float]) -> None:
+        """Constructs an ArrayList object from a user-specified list of numbers.
+
+        Args:
+            values (list[float]): The user-specified list of numbers
+        """
+        self._size = len(values)
+        
+    def __len__(self) -> int:
+        """Returns the size of this ArrayList.
+
+        Returns:
+            int: The size of this ArrayList
+        """
+        return self._size
