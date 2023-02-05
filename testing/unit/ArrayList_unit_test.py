fac0ea02e8a58c6d554f9f7b7ea5e38a0ea9a800
Delario4994 <denance@davidson.edu>
2023-02-04 21:53:00 -0500

    wrote unit tests for printing ArrayLists

--- testing/unit/ArrayList_unit_test.py
+++ testing/unit/ArrayList_unit_test.py
@@ -7,6 +7,7 @@ Date: January 24, 2023 - January 28, 2023
 """
 
 # Standard library imports
+import numpy as np
 import sys
 
 # Accessing project directories
@@ -17,7 +18,7 @@ sys.path.append(WSL_FILEPATH_TO_TESTING) # helpers
 
 # Local application imports
 from ArrayList import ArrayList
-from helpers import rand_array_list
+from helpers import rand_array_list, ndarray_of_first_ints
 
 
 class TestGettingSize:
@@ -25,25 +26,82 @@ class TestGettingSize:
     """
     def test_get_size_of_empty_ArrayList(self):
         empty_arraylist = rand_array_list(0)
+        
         size = len(empty_arraylist)
+        
         assert size == 0
     
     def test_get_size_of_ArrayList_of_one_int(self):
         arraylist_of_one_int = rand_array_list(1)
+        
         size = len(arraylist_of_one_int)
+        
         assert size == 1
     
     def test_get_size_of_ArrayList_of_ten_ints(self):
         arraylist_of_ten_ints = rand_array_list(10)
+        
         size = len(arraylist_of_ten_ints)
+        
         assert size == 10
     
     def test_get_size_of_ArrayList_of_one_hundred_ints(self):
         arraylist_of_one_hundred_ints = rand_array_list(100)
+        
         size = len(arraylist_of_one_hundred_ints)
+        
         assert size == 100
     
     def test_get_size_of_ArrayList_of_ten_thousand_ints(self):
         arraylist_of_ten_thousand_ints = rand_array_list(10000)
+        
         size = len(arraylist_of_ten_thousand_ints)
+        
         assert size == 10000
+        
+class TestPrinting:
+    def test_print_empty_ArrayList(self, capsys):
+        empty_arraylist = rand_array_list(0)
+        empty_ndarray = ndarray_of_first_ints(0)
+        
+        print(empty_arraylist)
+        output_from_empty_arraylist = capsys.readouterr()[0]
+        
+        assert output_from_empty_arraylist == str(empty_ndarray)
+        
+    
+    def test_print_ArrayList_of_one_int(self, capsys):
+        arraylist_of_one_int = rand_array_list(1)
+        ndarray_of_one_int = ndarray_of_first_ints(1)
+        
+        print(arraylist_of_one_int)
+        output_from_arraylist_of_one_int = capsys.readouterr()[0]
+        
+        assert output_from_arraylist_of_one_int == str(ndarray_of_one_int)
+    
+    def test_print_ArrayList_of_ten_ints(self, capsys):
+        arraylist_of_ten_ints = rand_array_list(10)
+        ndarray_of_ten_ints = ndarray_of_first_ints(10)
+        
+        print(arraylist_of_ten_ints)
+        output_from_arraylist_of_ten_ints = capsys.readouterr()[0]
+        
+        assert output_from_arraylist_of_ten_ints == str(ndarray_of_ten_ints)
+    
+    def test_print_ArrayList_of_one_hundred_ints(self, capsys):
+        arraylist_of_one_hundred_ints = rand_array_list(100)
+        ndarray_of_one_hundred_ints = ndarray_of_first_ints(100)
+        
+        print(arraylist_of_one_hundred_ints)
+        output_from_arraylist_of_one_hundred_ints = capsys.readouterr()[0]
+        
+        assert output_from_arraylist_of_one_hundred_ints == str(ndarray_of_one_hundred_ints)
+    
+    def test_print_ArrayList_of_ten_thousand_ints(self, capsys):
+        arraylist_of_ten_thousand_ints = rand_array_list(10000)
+        ndarray_of_ten_thousand_ints = ndarray_of_first_ints(10000)
+        
+        print(arraylist_of_ten_thousand_ints)
+        output_from_arraylist_of_ten_thousand_ints = capsys.readouterr()[0]
+        
+        assert output_from_arraylist_of_ten_thousand_ints == str(ndarray_of_ten_thousand_ints)
