0fe0ad72528183ab13ddb5cd9ab1463392cf6460
Delario4994 <denance@davidson.edu>
2023-02-15 22:04:44 -0500

    wrote end-to-end tests for finding the maximum of an ArrayList

--- testing/end-to-end/Array_List_endtoend_test.py
+++ testing/end-to-end/Array_List_endtoend_test.py
@@ -22,6 +22,8 @@ from helpers import *
 
 
 class TestFindingMinimum:
+    """Uses GettingValue and GettingSize modules.
+    """
     def test_find_min_in_ArrayList_of_one_int(self):
         pylist_of_one_int = rand_pylist(1)
         arraylist_of_one_int = ArrayList(pylist_of_one_int)
@@ -53,3 +55,38 @@ class TestFindingMinimum:
         min_of_arraylist = arraylist_of_ten_thousand_ints.min()
         
         assert min_of_arraylist == min(pylist_of_ten_thousand_ints)
+        
+class TestFindingMaximum:
+    """Uses GettingValue and GettingSize modules.
+    """
+    def test_find_max_in_ArrayList_of_one_int(self):
+        pylist_of_one_int = rand_pylist(1)
+        arraylist_of_one_int = ArrayList(pylist_of_one_int)
+        
+        max_of_arraylist = arraylist_of_one_int.max()
+        
+        assert max_of_arraylist == max(pylist_of_one_int)
+        
+    def test_find_max_in_ArrayList_of_ten_ints(self):
+        pylist_of_ten_ints = rand_pylist(10)
+        arraylist_of_ten_ints = ArrayList(pylist_of_ten_ints)
+        
+        max_of_arraylist = arraylist_of_ten_ints.max()
+        
+        assert max_of_arraylist == max(pylist_of_ten_ints)
+        
+    def test_find_max_in_ArrayList_of_one_hundred_ints(self):
+        pylist_of_one_hundred_ints = rand_pylist(100)
+        arraylist_of_one_hundred_ints = ArrayList(pylist_of_one_hundred_ints)
+        
+        max_of_arraylist = arraylist_of_one_hundred_ints.max()
+        
+        assert max_of_arraylist == max(pylist_of_one_hundred_ints)
+        
+    def test_find_max_in_ArrayList_of_ten_thousand_ints(self):
+        pylist_of_ten_thousand_ints = rand_pylist(10000)
+        arraylist_of_ten_thousand_ints = ArrayList(pylist_of_ten_thousand_ints)
+        
+        max_of_arraylist = arraylist_of_ten_thousand_ints.max()
+        
+        assert max_of_arraylist == max(pylist_of_ten_thousand_ints)
