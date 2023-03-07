8355cf49f8f839e02b0aa5e7a537b7f8aa31d506
Delario4994 <denance@davidson.edu>
2023-03-07 16:07:15 -0500

    implemented __contains__ magic method for ArrayLists

--- src/ArrayList.py
+++ src/ArrayList.py
@@ -138,11 +138,22 @@ class ArrayList:
         """
         return len(self) + negative_index
     
-    def __str__(self) -> NDArray[np.int_]:
-        """Returns the string representation of this ArrayList.
-        """
-        return str(self._values[:len(self)])
+    def __contains__(self, target: int) -> bool:
+        """Returns true iff this ArrayList contains a 
+        user-specified value.
 
+        Args:
+            target (int): The user-specified value to search for
+
+        Returns:
+            bool: True iff this ArrayList contains the
+            user-specified value
+        """
+        for index in range(len(self)):
+            if self[index] == target:
+                return True
+        return False
+    
     def __eq__(self, lst_to_compare: Union[list[int], ArrayList]) -> bool:
         """Returns true iff this ArrayList and a 
         user-specified Python list or second ArrayList contain
@@ -167,6 +178,11 @@ class ArrayList:
                 return False
         return True    
     
+    def __str__(self) -> NDArray[np.int_]:
+        """Returns the string representation of this ArrayList.
+        """
+        return str(self._values[:len(self)])
+    
     def append(self, value: int) -> None:
         """Appends a user-specified value to the end of this
         ArrayList.
@@ -180,7 +196,7 @@ class ArrayList:
             self._values = lengthened_arr_of_values
             self._capacity = new_capacity
             
-        self._values[self._next] = value
+        self[len(self)] = value
         self._next += 1        
             
     def remove(self, index: int) -> None:
@@ -190,7 +206,7 @@ class ArrayList:
         Args:
             index (int): The user-specified index
         """
-        removed_value = self._values[index]
+        removed_value = self[index]
         
         if index >= 0:
             self._shift_values_left_to_index(index)
@@ -209,8 +225,8 @@ class ArrayList:
             bound (int): The user-specified index
         """
         for index in range(bound,len(self)-1):
-            self._values[index] = self._values[index+1]
-        self._values[len(self)-1] = VALUE_FOR_INDEX_PAST_CAPACITY
+            self[index] = self[index+1]
+        self[len(self)-1] = VALUE_FOR_INDEX_PAST_CAPACITY
         
         self._next -= 1
     
@@ -221,7 +237,7 @@ class ArrayList:
             int: The maximum value in this ArrayList
         """
         index_of_max = self._get_index_of_max(0,len(self)-1)
-        return self._values[index_of_max]
+        return self[index_of_max]
     
     def _get_index_of_max(self, start: int, end: int) -> int:
         """Returns the index of the largest value in a
@@ -240,7 +256,7 @@ class ArrayList:
         index_of_max = start
         
         for index in range(start+1,end+1):
-            if self._values[index] > self._values[index_of_max]:
+            if self[index] > self[index_of_max]:
                 index_of_max = index
         return index_of_max
     
@@ -251,7 +267,7 @@ class ArrayList:
             int: The minimum value in this ArrayList
         """
         index_of_min = self._get_index_of_min(0,len(self)-1)
-        return self._values[index_of_min]
+        return self[index_of_min]
     
     def _get_index_of_min(self, start: int, end: int) -> int:
         """Returns the index of the smallest value in a
@@ -270,7 +286,7 @@ class ArrayList:
         index_of_min = start
         
         for index in range(start+1,end+1):
-            if self._values[index] < self._values[index_of_min]:
+            if self[index] < self[index_of_min]:
                 index_of_min = index
         return index_of_min
     
@@ -299,7 +315,7 @@ class ArrayList:
             
             reversed_array_list = copy_of_array_list
         """
-        size = self._next
+        size = len(self)
         
         for index in range(size//2):
             mirror_index = size-index-1
@@ -313,7 +329,7 @@ class ArrayList:
             index_a (int): The first user-specified index
             index_b (int): The second user-specified index
         """
-        self._values[index_a], self._values[index_b] = self._values[index_b], self._values[index_a]
+        self[index_a], self[index_b] = self[index_b], self[index_a]
      
     def selection_sort(self, reverse: bool = False) -> None:
         """Sorts the values in this ArrayList using selection
@@ -550,20 +566,3 @@ class ArrayList:
             merged_ptr += 1
             
         return merged
-    
-    def contains(self, value: int) -> bool:
-        """Returns true iff this ArrayList contains a 
-        user-specified value
-
-        Args:
-            value (int): The user-specified value
-
-        Returns:
-            bool: True iff this ArrayList contains the 
-        user-specified value
-        """
-        size = len(self)
-        for index in range(size):
-            if self._values[index] == value:
-                return True
-        return False
