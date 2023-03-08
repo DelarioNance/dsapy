e1bbfbb97a4b5fcad0bf314272abb47c9ebd34c6
Delario4994 <denance@davidson.edu>
2023-03-07 19:44:28 -0500

    parametrized BubbleSorting end-to-end tests for ArrayLists

--- testing/end-to-end/Array_List_endtoend_test.py
+++ testing/end-to-end/Array_List_endtoend_test.py
@@ -110,7 +110,7 @@ class TestReversing:
 class TestSelectionSorting:
     """Unlike the end-to-end test methods for merge_sort and 
     quicksort, the selection_sort test methods on ArrayLists 
-    with ten-thousand ints are ommited because selction_sort 
+    with ten-thousand ints are ommited because selection_sort 
     runs for a long time on large ArrayLists due to 
     selection_sort's worst-case running time of O(n^2).
     """
@@ -164,67 +164,60 @@ class TestSelectionSorting:
         
         
 class TestBubbleSorting:
-    """Uses GettingValue, GettingSize, and CheckingIfEqual modules.
-    
-    Unlike the end-to-end test methods for merge_sort and 
-    quicksort, the bubblesort test methods on ArrayLists with
-    ten-thousand ints are ommited because bubblesort runs 
-    for a long time on large ArrayLists due to bubblesort's
-    worst-case running time of O(n^2).
+    """Unlike the end-to-end test methods for merge_sort and 
+    quicksort, the bubblesort test methods on ArrayLists 
+    with ten-thousand ints are ommited because bubblesort 
+    runs for a long time on large ArrayLists due to 
+    bubblesort's worst-case running time of O(n^2).
     """
-    def test_bubblesort_ArrayList_of_one_int_in_ascending_order(self):
-        pylist_of_one_int = rand_pylist(1)
-        arraylist_of_one_int = ArrayList(pylist_of_one_int)
+    @pytest.mark.parametrize("num_of_ints", [
+            pytest.param(1, id='one int'),
+            pytest.param(10, id='ten ints'),
+            pytest.param(100, id='one-hundred ints'),
+            pytest.param(1000, id='one-thousand ints'),
+            # pytest.param(10000, id='ten-thousand ints') SLOW
+    ])
+    
+    def test_bubblesort_ArrayList_in_ascending_order(self, num_of_ints: int) -> None:
+        """Tests if a user can sort the values in an ArrayList
+        of a user-specified number of random ints, in ascending
+        order.
+
+        Args:
+            num_of_ints (int): The user-specified number of 
+            random ints
+        """
+        pytest = rand_pylist(num_of_ints)
+        arraylist = ArrayList(pytest)
         
-        pylist_of_one_int.sort()
-        arraylist_of_one_int.bubblesort()
+        arraylist.bubblesort(reverse=False)
         
-        assert arraylist_of_one_int == pylist_of_one_int
+        assert arraylist == sorted(pytest, reverse=False)
+    
+    @pytest.mark.parametrize("num_of_ints", [
+            pytest.param(1, id='one int'),
+            pytest.param(10, id='ten ints'),
+            pytest.param(100, id='one-hundred ints'),
+            pytest.param(1000, id='one-thousand ints'),
+            # pytest.param(10000, id='ten-thousand ints') SLOW
+    ])
+    
+    def test_bubblesort_ArrayList_in_descending_order(self, num_of_ints) -> None:
+        """Tests if a user can sort the values in an ArrayList
+        of a user-specified number of random ints, in descending
+        order.
+
+        Args:
+            num_of_ints (int): The user-specified number of 
+            random ints
+        """
+        pytest = rand_pylist(num_of_ints)
+        arraylist = ArrayList(pytest)
         
-    def test_bubblesort_ArrayList_of_ten_ints_in_ascending_order(self):
-        pylist_of_ten_ints = rand_pylist(10)
-        arraylist_of_ten_ints = ArrayList(pylist_of_ten_ints)
+        arraylist.bubblesort(reverse=False)
         
-        pylist_of_ten_ints.sort()
-        arraylist_of_ten_ints.bubblesort()
+        assert arraylist == sorted(pytest, reverse=False)
         
-        assert arraylist_of_ten_ints == pylist_of_ten_ints
-        
-    def test_bubblesort_ArrayList_of_one_hundred_ints_in_ascending_order(self):
-        pylist_of_one_hundred_ints = rand_pylist(100)
-        arraylist_of_one_hundred_ints = ArrayList(pylist_of_one_hundred_ints)
-        
-        pylist_of_one_hundred_ints.sort()
-        arraylist_of_one_hundred_ints.bubblesort()
-        
-        assert arraylist_of_one_hundred_ints == pylist_of_one_hundred_ints
-        
-    def test_bubblesort_ArrayList_of_one_int_in_descending_order(self):
-        pylist_of_one_int = rand_pylist(1)
-        arraylist_of_one_int = ArrayList(pylist_of_one_int)
-        
-        pylist_of_one_int.sort(reverse=True)
-        arraylist_of_one_int.bubblesort(True)
-        
-        assert arraylist_of_one_int == pylist_of_one_int
-        
-    def test_bubblesort_ArrayList_of_ten_ints_in_descending_order(self):
-        pylist_of_ten_ints = rand_pylist(10)
-        arraylist_of_ten_ints = ArrayList(pylist_of_ten_ints)
-        
-        pylist_of_ten_ints.sort(reverse=True)
-        arraylist_of_ten_ints.bubblesort(True)
-        
-        assert arraylist_of_ten_ints == pylist_of_ten_ints
-        
-    def test_bubblesort_ArrayList_of_one_hundred_ints_in_descending_order(self):
-        pylist_of_one_hundred_ints = rand_pylist(100)
-        arraylist_of_one_hundred_ints = ArrayList(pylist_of_one_hundred_ints)
-        
-        pylist_of_one_hundred_ints.sort(reverse=True)
-        arraylist_of_one_hundred_ints.bubblesort(True)
-        
-        assert arraylist_of_one_hundred_ints == pylist_of_one_hundred_ints
         
 class TestInsertionSorting:
     """Uses GettingValue, GettingSize, and CheckingIfEqual modules.
